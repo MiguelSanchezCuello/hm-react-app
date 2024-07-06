9d838747c0ab603f96e19708e35cd40c7642e751
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-05 21:38:39 -0400

    Modificado el CardBody y Agregadas las propiedades adicionales del CardBody

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Card from "./components/Card";
+import Card, { CardBody } from "./components/Card";
 
 function App() {
-  return <Card body={"HOla Mundo"} />;
+  return <CardBody title={"HOla Mundo"} text={"El texto del componente"} />;
 }
 
 export default App;
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,33 +1,30 @@
 interface Props {
-    body: string
+  body: string;
 }
 function Card(props: Props) {
-    const { body } = props;
-  return ( 
+  const { body } = props;
+  return (
     <div
       className="card"
       style={{
         width: "350px",
       }}
     >
-      <div className="card-body">
-        {body}
-      </div>
+      <div className="card-body">{body}</div>
     </div>
   );
 }
+interface CardBodyProps {
+  title: string;
+  text?: string;
+}
 
-export function CardBody() {
+export function CardBody(props: CardBodyProps) {
+  const { title, text } = props;
   return (
     <>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
     </>
   );
 }
