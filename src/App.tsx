995d2b7f9e715b2bffc1193ab40b16aac8064f67
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-07 19:45:13 -0400

    Today was just a day of rewatching some of the vids

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,29 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const list = ["Goku", "Tanjiro", "Miguel"];
+  const list: string[] = ["Goku", "Tanjiro", "Miguel"];
+  let btnClass: string = "btn btn-primary";
 
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo :" + elemento);
   };
+
+  const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect} />
+  ) : (
+    "Sin elementos para mostrar"
+  );
+
+  const handleClick = () => {
+    btnClass = "btn btn-secondary"
+  }
+
   return (
     <Card>
       {/* {list.length !== 0 && "mi lista"}  ejemplo de como funciona un valor falsy */}
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} onSelect={handleSelect} />
+      {contenido}
+      <button type="button" className={btnClass} onClick={handleClick}>Primary</button>
     </Card>
   );
 }
