1b7964b19d174f7138a80bbbbf35895575a045ce
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-08 22:05:30 -0400

    CSS en JavaScript ============ En este modo el CSS se encuentra dentro del mismo archivo del componente. Una ventaja de este modo es que si te llevas el componente a otro proyecto este se va con todo y sus estilos. Otra ventaja es que cuando se genera dentro del js si inspeccionamos el elemento podemos ver que las clases tienen un nombre autogenerado que no permitira que las clases de un elemento y otro choquen entre si y que posean su individualidad. y ya no tendriamos que aplicar ningun estilo y el codigo estaria mas limpio ya que solamente le agregamos funcionalidades en el jsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,11 @@
 import { ReactNode } from "react";
-import styles from "./Button.module.css";
+// import styles from "./Button.module.css";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  padding: 25px 30px;
+  background-color: red;
+`;
 
 type Props = {
   children: ReactNode;
@@ -7,24 +13,36 @@ type Props = {
   onClick: () => void;
 };
 
-console.log(styles);
+// Codigo usando CSS en JS
 function Button({ children, isLoading, onClick }: Props) {
-  const className = [
-    `btn btn-${isLoading ? "secondary" : "primary"}`,
-    styles.button,
-    styles.padded,
-  ].join(" ");
-  return (
-    <button
-      disabled={isLoading}
-      onClick={onClick}
-      type="button"
-      className={className}
-      // className={[styles.button, styles.padded].join(" ")}
-    >
-      {isLoading ? "Cargando" : children}
-    </button>
+  return ( 
+  <Btn onClick={onClick} disabled={isLoading} type="button">
+    {isLoading ? "Cargando" : children}
+  </Btn> 
   );
 }
+
+
+
+// Codigo con modules
+// console.log(styles);
+// function Button({ children, isLoading, onClick }: Props) {
+//   const className = [
+//     `btn btn-${isLoading ? "secondary" : "primary"}`,
+//     styles.button,
+//     styles.padded,
+//   ].join(" ");
+//   return (
+//     <button
+//       disabled={isLoading}
+//       onClick={onClick}
+//       type="button"
+//       className={className}
+//       // className={[styles.button, styles.padded].join(" ")}
+//     >
+//       {isLoading ? "Cargando" : children}
+//     </button>
+//   );
+// }
 
 export default Button;
